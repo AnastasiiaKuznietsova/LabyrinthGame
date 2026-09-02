2396bb3d8629fffd4a118d01d0b5279323e24965
Anastasiia <Anastasiia.Kuznietsova@edu.bib.de>
2026-09-02 11:43:48 +0200

    Add basic Card model

---
+++ Models/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthGame.Models
+{
+    public class Card
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string ImagePath { get; set; } = string.Empty;
+
+        public double ImageAngle { get; set; }
+    }
+}
