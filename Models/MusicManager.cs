0ece8fea6549c79efa664d323b1d5c20c18f2dba
Anastasiia <Anastasiia.Kuznietsova@edu.bib.de>
2026-09-03 13:34:16 +0200

    Add music manager

---
+++ Models/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Media; // Zum Abspielen von Audiodateien verwendet wird
+
+namespace LabyrinthGame.Models
+{
+    internal class MusicManager
+    {
+        // Eigenschaft für SoundPlayer
+        public SoundPlayer Player { get; private set; }
+
+        // Der Konstruktor erstellt ein SoundPlayer-Objekt, weist es der Eigenschaft Player zu und ruft PlayMusic() auf, um die Musik abzuspielen.
+        public MusicManager()
+        {
+            this.Player = new SoundPlayer("C:\\repos\\LabyrinthGame\\Resources\\Music\\music1.wav");
+            //this.PlayMusic();
+        }
+
+        // Methode zum Abspielen der Musik
+        public void PlayMusic()
+        {
+            this.Player.Play();
+        }
+    }
+}
