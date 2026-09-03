6414e43d55117fe2f204bfe0ca7090658c25294f
Anastasiia <Anastasiia.Kuznietsova@edu.bib.de>
2026-09-03 13:38:49 +0200

    Add local game field setup

--- Models/LocalMultiplayerGameManager.cs
+++ Models/LocalMultiplayerGameManager.cs
@@ -26,5 +26,29 @@ namespace LabyrinthGame.Models
             this.SetRandomStartPlayer();
             this.CurrentPlayer.IsAbleToPlaceCard = true;
         }
+
+        // Platziere 4 zufällige Karten aus dem CardStack in der Mitte des GameField (einmal zu Beginn jedes Spiels aufgerufen)
+        public void InitializeGameField()
+        {
+            this.GameField.AddCard(50, 50, GetCardFromCardStack());
+            this.GameField.AddCard(50, 51, GetCardFromCardStack());
+            this.GameField.AddCard(51, 50, GetCardFromCardStack());
+            this.GameField.AddCard(51, 51, GetCardFromCardStack());
+        }
+
+        // Setzt zufällig Player1 oder Player2 als CurrentPlayer.
+        public void SetRandomStartPlayer()
+        {
+            Random random = new Random();
+            this.CurrentPlayer = random.Next(1, 3) == 1 ? this.Player1 : this.Player2;
+        }
+
+        // Gibt die erste Karte aus dem CardStack zurück und löscht sie
+        public Card GetCardFromCardStack()
+        {
+            var outputCard = this.CardStack.First();
+            this.CardStack.Pop();
+            return outputCard;
+        }
     }
 }
