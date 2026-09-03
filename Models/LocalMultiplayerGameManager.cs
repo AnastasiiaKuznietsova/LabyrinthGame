df0fd8fca7a2c91732fc1e5af7b0ecc733535871
Anastasiia <Anastasiia.Kuznietsova@edu.bib.de>
2026-09-03 13:37:02 +0200

    Add local game manager structure

---
+++ Models/LocalMultiplayerGameManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabyrinthGame.Models
+{
+    internal class LocalMultiplayerGameManager
+    {
+        // Jeder LocalMultiplayerGameManager hat einen CardStack, ein GameField und 2 Spieler
+        public Stack<Card> CardStack { get; set; }
+        public GameField GameField { get; set; }
+        public IPlayer Player1 { get; set; }
+        public IPlayer Player2 { get; set; }
+        public IPlayer CurrentPlayer { get; set; }
+
+        // Der Konstruktor von LocalMultiplayerGameManager benötigt 2 Profile, um die Objekte Player1 und Player2 zu erstellen
+        public LocalMultiplayerGameManager(Profile pProfile1, Profile pProfile2)
+        {
+            this.CardStack = GenerateCardStack();
+            this.GameField = new();
+            this.InitializeGameField();
+            this.Player1 = new HumanPlayer(pProfile1, GetCardFromCardStack(), GetCardFromCardStack());
+            this.Player2 = new HumanPlayer(pProfile2, GetCardFromCardStack(), GetCardFromCardStack());
+            this.SetRandomStartPlayer();
+            this.CurrentPlayer.IsAbleToPlaceCard = true;
+        }
+    }
+}
