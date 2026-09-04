178b2de325f97baad8668fcd77ba3a8c54f2ab3f
Anastasiia <Anastasiia.Kuznietsova@edu.bib.de>
2026-09-04 12:55:25 +0200

    Add main view model commands

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LabyrinthGame.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ICommand TrophyCommand { get; }
+        public ICommand ExitCommand { get; }
+        public ICommand SettingsCommand { get; }
+        public ICommand ChangeProfileCommand { get; }
+        public ICommand SecretCommand { get; }
+        private ProgramManager ProgramManager;
+
+    }
+}
