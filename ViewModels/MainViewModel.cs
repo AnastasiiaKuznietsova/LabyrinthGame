78973a2cbaa03fd213c2e93bda98e5b2f7287028
Anastasiia <Anastasiia.Kuznietsova@edu.bib.de>
2026-09-04 12:56:51 +0200

    Add main view model actions

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -14,5 +14,27 @@ namespace LabyrinthGame.ViewModels
         public ICommand SecretCommand { get; }
         private ProgramManager ProgramManager;
 
+        public MainViewModel()
+        {
+            TrophyCommand = new RelayCommand(param => OpenTrophyWindow(param));
+            ExitCommand = new RelayCommand(param => OpenExitWindow(param));
+            SettingsCommand = new RelayCommand(param => OpenSettingsWindow(param));
+            ChangeProfileCommand = new RelayCommand(param => OpenChangeProfileWindow(param));
+            SecretCommand = new RelayCommand(param => EnableSecret(param));
+            ProgramManager = ProgramManager.Instance;
+        }
+
+        private void EnableSecret(object? parameter)
+        {
+            ProgramManager.SecretMode = true;
+        }
+
+        private void OpenExitWindow(object? parameter)
+        {
+            var exitWindow = new ExitWindow();
+            exitWindow.Show();
+            CloseCurrentWindow(parameter);
+        }
+
     }
 }
