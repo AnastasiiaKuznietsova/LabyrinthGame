2b490a29122db12af04e7a35c92b057e6c3d5164
Anastasiia <Anastasiia.Kuznietsova@edu.bib.de>
2026-09-04 12:50:41 +0200

    Add RelayCommand execution

--- ViewModels/RelayCommand.cs
+++ ViewModels/RelayCommand.cs
@@ -13,6 +13,15 @@ namespace LabyrinthGame
 
         private readonly Action<object?> _execute;
         private readonly Func<object?, bool>? _canExecute;
+
+        public bool CanExecute(object? parameter) => _canExecute == null || _canExecute(parameter);
+
+        public void Execute(object? parameter) => _execute(parameter);
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
     }
-   
 }
