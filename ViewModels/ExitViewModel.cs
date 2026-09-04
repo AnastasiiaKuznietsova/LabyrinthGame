825dc6ccace06cca804fe5103bbfe90253c28c96
Anastasiia <Anastasiia.Kuznietsova@edu.bib.de>
2026-09-04 13:02:28 +0200

    Add exit view model navigation

--- ViewModels/ExitViewModel.cs
+++ ViewModels/ExitViewModel.cs
@@ -14,5 +14,27 @@ namespace LabyrinthGame.ViewModels
         public ICommand YesCommand { get; }
         private ProgramManager ProgramManager;
 
+        public ExitViewModel()
+        {
+            SettingsCommand = new RelayCommand(param => OpenSettings(param));
+            TrophyCommand = new RelayCommand(param => OpenTrophy(param));
+            HomeCommand = new RelayCommand(param => OpenHome(param));
+            NoCommand = new RelayCommand(param => OpenHome(param));
+            YesCommand = new RelayCommand(param => ExitApplication());
+            ProgramManager = ProgramManager.Instance;
+        }
+
+        private void OpenSettings(object? parameter)
+        {
+            var settingsWindow = new SettingsWindow();
+            settingsWindow.Show();
+        }
+
+        private void OpenTrophy(object? parameter)
+        {
+            var trophyWindow = new TrophyWindow();
+            trophyWindow.Show();
+        }
+
     }
 }
