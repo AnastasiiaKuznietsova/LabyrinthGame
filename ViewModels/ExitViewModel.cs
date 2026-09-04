e7c77755fa5cc8088ae0672dc319c3b7839f5f13
Anastasiia <Anastasiia.Kuznietsova@edu.bib.de>
2026-09-04 13:01:39 +0200

    Add exit view model structure

---
+++ ViewModels/ExitViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LabyrinthGame.ViewModels
+{
+    public class ExitViewModel : INotifyPropertyChanged
+    {
+        public ICommand SettingsCommand { get; }
+        public ICommand TrophyCommand { get; }
+        public ICommand HomeCommand { get; }
+        public ICommand NoCommand { get; }
+        public ICommand YesCommand { get; }
+        private ProgramManager ProgramManager;
+
+    }
+}
